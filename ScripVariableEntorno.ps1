664a0acef38987411eb5f48fdaafb329bd0a9afe
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-07-12 02:16:13 +0200

    añadido script para crear variable de entorno

---
+++ ScripVariableEntorno.ps1
@@ -0,0 +1,15 @@
+﻿
+if (Test-Path 'env:VUELING_HOME3') 
+{ 
+    "Variable de entorno VUELING_HOME ya existente:"
+    Get-Childitem env:VUELING_HOME
+} 
+else 
+{
+    [Environment]::SetEnvironmentVariable('VUELING_HOME3', 'C:\Users\G1\Documents', 'User')
+    "Nueva variable de entorno VUELING_HOME2 creada correctamente"
+}
+
+
+"http://www.itprotoday.com/management-mobility/powershell-one-liner-creating-and-modifying-environment-variable"
+"https://ss64.com/ps/syntax-env.html"
