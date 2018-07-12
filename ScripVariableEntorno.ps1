ed0acd3ddf383c6720655646e51382f7a2abd284
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-07-12 04:09:54 +0200

    Modificación del archivo .ps1

--- ScripVariableEntorno.ps1
+++ ScripVariableEntorno.ps1
@@ -1,4 +1,14 @@
 ﻿
+Function Mostrar-MensajeCuadroDialogo {
+    Param
+    (
+    [string]$Mensaje, 
+    [string]$Titulo, 
+    [System.Windows.Forms.MessageBoxButtons]$Botones, 
+    [System.Windows.Forms.MessageBoxIcon]$Icono
+    )
+    return [System.Windows.Forms.MessageBox]::Show($Mensaje, $Titulo, $Botones, $Icono)
+}
 if (Test-Path 'env:VUELING_HOME3') 
 { 
     "Variable de entorno VUELING_HOME ya existente:"
@@ -7,8 +17,10 @@ if (Test-Path 'env:VUELING_HOME3')
 else 
 {
     [Environment]::SetEnvironmentVariable('VUELING_HOME3', 'C:\Users\G1\Documents', 'User')
-    "Nueva variable de entorno VUELING_HOME2 creada correctamente"
+    "Nueva variable de entorno VUELING_HOME3 creada correctamente"
+    Mostrar-MensajeCuadroDialogo -Mensaje "Ha finalizado correctamente el proceso" -Titulo "Información" -Botones OK -Icono Information
 }
+
 
 
 "http://www.itprotoday.com/management-mobility/powershell-one-liner-creating-and-modifying-environment-variable"
