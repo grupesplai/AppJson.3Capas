c57feb047e70c69e374273cd1e5c4e7a144c277d
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-07-13 17:34:13 +0200

    Añadido fichero de recursos con algunas claves y algunos valores

--- FileServer.Presentation.WinSite/Form1.cs
+++ FileServer.Presentation.WinSite/Form1.cs
@@ -7,12 +7,15 @@ using FileServer.Infrastructure.Repository_DAO_;
 using FileServer.Infrstructure.Repository_DAO_;
 using log4net;
 using System.Diagnostics;
+using System.Resources;
+using System.Configuration;
 
 namespace FileServer.Presentation.WinSite
 {
     public partial class Form1 : Form
     {
         private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        string resxFile = ConfigurationManager.AppSettings["resx"];
         public Form1()
         {
             InitializeComponent();
@@ -20,30 +23,33 @@ namespace FileServer.Presentation.WinSite
 
         public void btnreg_Click(object sender, EventArgs e)
         {
-            try
+            using (ResXResourceSet resxSet = new ResXResourceSet(resxFile))
             {
-                int p = 0;
-                var num = 5 / p;
-            }
-            catch (Exception ex)
-            {
-                LogManager.GetLogger("EmailLogger").Error(ex);
-            }
-            log.Debug(string.Format(@"Datos introducidos del alumno --> ID = {0} | Nombre = {1} | Apellidos = {2} | DNI = {3}",
-                      txtid.Text, txtnombre.Text, txtapellidos.Text, txtdni.Text));
-            AlumnoRepository AlumnoRepositorio = new AlumnoRepository();
-            if (!(int.TryParse(txtid.Text, out int id)))
-            {
-                MessageBox.Show(id + "No es un tipo de dato correcto");
-            }
-            else
-            {   
-                Alumno alum = new Alumno(Convert.ToInt32(txtid.Text), txtnombre.Text, txtapellidos.Text,
-                txtdni.Text);
-                var alumnoCreado = AlumnoRepositorio.Add(alum, FileManager.FilePath(comboBox1.SelectedIndex, comboBox2.SelectedIndex));
-                //pendiente de implementar para que SOLO salga en caso de registrarlo correctamente, de momento se muestra siempre
-                FileManager.Validation(alum,alumnoCreado);
-                
+                try
+                {
+                    int p = 0;
+                    var num = 5 / p;
+                }
+                catch (Exception ex)
+                {
+                    LogManager.GetLogger("EmailLogger").Error(ex);
+                }
+                log.Debug(string.Format(@"Datos introducidos del alumno --> ID = {0} | Nombre = {1} | Apellidos = {2} | DNI = {3}",
+                          txtid.Text, txtnombre.Text, txtapellidos.Text, txtdni.Text));
+                AlumnoRepository AlumnoRepositorio = new AlumnoRepository();
+                if (!(int.TryParse(txtid.Text, out int id)))
+                {
+                    MessageBox.Show(id + "\t--> No es un tipo de dato correcto");
+                }
+                else
+                {
+                    Alumno alum = new Alumno(Convert.ToInt32(txtid.Text), txtnombre.Text, txtapellidos.Text,
+                    txtdni.Text);
+                    var alumnoCreado = AlumnoRepositorio.Add(alum, FileManager.FilePath(comboBox1.SelectedIndex, comboBox2.SelectedIndex));
+                    //pendiente de implementar para que SOLO salga en caso de registrarlo correctamente, de momento se muestra siempre
+                    FileManager.Validation(alum, alumnoCreado);
+
+                }
             }
             foreach (TextBox tb in this.Controls.OfType<TextBox>().ToArray())
                 tb.Clear();
@@ -60,15 +66,18 @@ namespace FileServer.Presentation.WinSite
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string cmdText = @"C:\Users\G1\source\repos\FileServer\ScripVariableEntorno.ps1";
-            Process.Start(@"C:\windows\system32\windowspowershell\v1.0\powershell.exe ", cmdText);
-            comboBox1.Items.Add("App.config");
-            comboBox1.Items.Add("Variable de entorno");
-            comboBox2.Items.Add(".json");
-            comboBox2.Items.Add(".xml");
-            comboBox2.Items.Add(".txt");
-            comboBox1.SelectedIndex = 0;
-            comboBox2.SelectedIndex = 0;
+            using (ResXResourceSet resxSet = new ResXResourceSet(resxFile))
+            {
+                string cmdText = @"C:\Users\G1\source\repos\FileServer\ScripVariableEntorno.ps1";
+                Process.Start(@"C:\windows\system32\windowspowershell\v1.0\powershell.exe ", cmdText);
+                comboBox1.Items.Add(resxSet.GetString("CBOX_APPCONFIG"));
+                comboBox1.Items.Add(resxSet.GetString("CBOX_VAR_ENT"));
+                comboBox2.Items.Add(resxSet.GetString("CBOX_JSON"));
+                comboBox2.Items.Add(resxSet.GetString("CBOX_TXT"));
+                comboBox2.Items.Add(resxSet.GetString("CBOX_XML"));
+                comboBox1.SelectedIndex = 0;
+                comboBox2.SelectedIndex = 0;
+            }
         }
     }
 }
